c6aa1acb9f1fbf6f99828dd14396973f8196cf98
thebrianemory <the@brianemory.com>
2018-12-13 08:44:15 -0800

    Add secret key base

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,8 @@ config :tilex, TilexWeb.Endpoint,
   http: [port: 8080],
   load_from_system_env: true,
   url: [host: "thebrianemory.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
