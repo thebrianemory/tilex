b62972d1aba3cc1745d743cd31ca8df038c1a910
thebrianemory <the@brianemory.com>
2018-12-14 16:58:21 -0800

    Remove transport

--- lib/tilex_web/channels/user_socket.ex
+++ lib/tilex_web/channels/user_socket.ex
@@ -6,7 +6,7 @@ defmodule TilexWeb.UserSocket do
   channel("text_converter", Tilex.TextConverterChannel)
 
   ## Transports
-  transport(:websocket, Phoenix.Transports.WebSocket)
+  # transport(:websocket, Phoenix.Transports.WebSocket)
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
